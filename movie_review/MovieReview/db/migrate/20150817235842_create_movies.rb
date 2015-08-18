b7afb277fbe5251619019fc7249da67fd37b9f22
Ian Horsman <ianphorsman@gmail.com>
2015-08-17 21:40:48 -0400

    added models

--- movie_review/MovieReview/db/migrate/20150817235842_create_movies.rb
+++ movie_review/MovieReview/db/migrate/20150817235842_create_movies.rb
@@ -1,12 +1,13 @@
 class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies do |t|
-        t.string        :name
+        t.string        :title
         t.text          :description
         t.integer       :duration
         t.datetime      :release_date
         t.string        :genre
         t.string        :director
+        t.string        :poster_url
       t.timestamps null: false
     end
   end
--- movie_review/MovieReview/db/migrate/20150817235846_create_reviews.rb
+++ movie_review/MovieReview/db/migrate/20150817235846_create_reviews.rb
@@ -2,7 +2,9 @@ class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
         t.string            :title
+        t.string            :name
         t.text              :review
+        t.integer           :rating
         t.string            :sentiment
       t.timestamps null: false
     end
